28334b6c45ba9ca4a9029de97ecf768421f05658
Saccharin01 <chousik01@gmail.com>
2024-06-19 19:36:27 +0900

    testField reFine

--- insert.js
+++ insert.js
@@ -1,8 +1,21 @@
-const insertValue = (db, tableName, colName, values)=>{
-  db.run(`INSERT INTO ${tableName} ( ${colName} ) VALUES (${values});`)
+const insertValue = (tableName, colName, values)=>{
+ `INSERT INTO ${tableName} (${colName}) VALUES (${values});`
 
-
-  console.log(values)
+ console.log(values)
+ console.log(typeof(values))
+ console.dir(values)
   }
 
-  module.exports = insertValue
+
+
+
+  insertValue("music", "title, singer, genre, year", "낙원, 정가이, game")
+
+
+
+  
+  
+  
+  
+  // module.exports = insertValue
+
