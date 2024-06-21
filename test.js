43d91af7544e29593e92dde3fe0b1e213d0f3672
Saccharin01 <chousik01@gmail.com>
2024-06-21 14:59:32 +0900

    how to use

--- test.js
+++ test.js
@@ -1,15 +1,26 @@
 const create = require('./create.js')
 const sqlite3 = require('sqlite3').verbose()
 const alterTable = require('./alter.js')
-const insertValue = require('./insert.js')
-const db = new sqlite3.Database('test.db')
-create(db, "music")
-console.dir(alterTable)
-console.log(alterTable)
+const insertValue = require('./insert copy.js')
+const db = new sqlite3.Database('item.db')
+create(db, "item")
+db.run(create("item"))
+// console.dir(alterTable)
+// console.log(alterTable)
 // alterTable(db,"music", "Music_index", "primary key")
-alterTable(db, "music", "title", "text not null")
-alterTable(db, "music", "singer", "text")
-alterTable(db, "music", "genre", "text")
-alterTable(db, "music", "year", "integer")
 
-insertValue(db,"music","title, singer, genre","낙원, 정가이, game")
+// insertValue("music","title, singer, genre","낙원, 정가이, game")
+
+db.serialize(()=>{
+  // db.run(alterTable("item", "name","text not null"))
+  // db.run(alterTable("item", "price","text"))
+  // db.run(alterTable("item", "isSell","integer"))
+  // db.run(alterTable("item", "weight","integer"))
+  // db.run(alterTable("item", "type","text"))
+  let stmt = db.prepare(`INSERT INTO item (name, price, isSell, weight, type) VALUES (?,?,?,?,?);`)
+  stmt.run("에어혼 (Airhorn)", 52, 0, 0, "toy")
+  
+})
+
+
+
